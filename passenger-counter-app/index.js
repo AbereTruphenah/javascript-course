bd9237b3f5938a2f6f1fb58c492dd409ac3480ee
Truphenah <145029217+AbereTruphenah@users.noreply.github.com>
2025-06-26 09:32:06 +0300

    Concatenating strings

--- passenger-counter-app/index.js
+++ passenger-counter-app/index.js
@@ -1,9 +1,9 @@
-let username = "per"
+// Create two variables, name and greeting. The name variable should store your name,
+// and the greeting should store e.g. "Hi, my name is "
+let name = "Truphena"
+let greeting = "Hi, my name is "
 
-// Create a variable, message, that stores the string: "You have three new notifications"
-let message = "You have three new notifications"
-console.log(message + ", " + username + "!")
-
-// Create a variable, messageToUser, that contains the message we have logged
-let messageToUser = message + ", " + username + "!"
-console.log(messageToUser)
+// Create a third variable, myGreeting, that contatenates the two strings
+// Log myGreeting to the console
+let myGreeting = greeting + name
+console.log(myGreeting)
