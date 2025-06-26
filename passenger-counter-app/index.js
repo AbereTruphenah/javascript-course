c202b6fe06161e464cf9ecb4de0b75a04dd7e7f3
Truphenah <145029217+AbereTruphenah@users.noreply.github.com>
2025-06-26 10:33:44 +0300

    Use "+=" for count increment, and .textContent for rendering

--- passenger-counter-app/index.js
+++ passenger-counter-app/index.js
@@ -1,15 +1,25 @@
-// Grab the welcome-el paragraph and store it in a variable called welcomeEl
-let welcomeEl = document.getElementById("welcome-el")
+let countEl = document.getElementById("count-el")
+// 1. Grab the save-el paragrah and store it in a variable called saveEl
+let saveEl = document.getElementById("save-el")
 
-// Create two variables (name & greeting) that contains your name
-// and the greeting we want to render on the page
-let name = "Truphena"
-let greeting = "Hello "
+console.log(countEl)
 
-// Render the welcome message using welcomeEl.innerText
-welcomeEl.innerText = greeting + name
+let count = 0
 
-// Add an emoji to the end! 👋
-// WRITE YOUR CODE BELOW HERE
-// HINT: count = count + 1
-welcomeEl.innerText += "👋" 
+function increment() {
+    // Change this to use the plus equal technique you've learned
+    count += 1
+    countEl.textContent = count
+}
+
+// 1. Create a function, save(), which logs out the count when it's called
+function save(){
+    // 2. Create a variable that contains both the count and the dash separator, i.e. "12 - "
+    let countStr = count + " - "
+    // 3. Render the variable in the saveEl using innerText
+    saveEl.textContent += countStr
+    // NB: Make sure to not delete the existing content of the paragraph
+    console.log(count)
+}
+save()
+
