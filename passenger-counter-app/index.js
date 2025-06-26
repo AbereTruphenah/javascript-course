1fd7d10a48f5b2b435153bcb73e27b4942cb73fe
Truphenah <145029217+AbereTruphenah@users.noreply.github.com>
2025-06-26 09:17:39 +0300

    Added save button

--- passenger-counter-app/index.js
+++ passenger-counter-app/index.js
@@ -13,5 +13,9 @@ function increment() {
     countEl.innerText = count
 }
 
-
+// 1. Create a function, save(), which logs out the count when it's called
+function save(){
+    console.log(count)
+}
+save()
 
