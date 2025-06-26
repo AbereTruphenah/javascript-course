f580d14bb39fbf1e3c932f791e6ecd1a877bb058
Truphenah <145029217+AbereTruphenah@users.noreply.github.com>
2025-06-26 07:50:56 +0300

    JavaScript: reassigning and incrementing

--- passenger-counter-app/index.js
+++ passenger-counter-app/index.js
@@ -1,8 +1,14 @@
-// 1. Create two variables, myAge and humanDogRatio
-let myAge = 10
-let humanDogRatio = 5
-// 2. Multiply the two together and store the result in myDogAge
-let myDogAge = myAge * humanDogRatio
-// 3. Log myDogAge to the console
-console.log(myDogAge)
+// Create a variable, bonusPoints. Initialize it as 50. Increase it to 100.
+// Decrease it down to 25, and then finally increase it to 70
+// Console.log the value after each step
+let bonusPoints = 50
+console.log(bonusPoints)
 
+bonusPoints = bonusPoints + 50
+console.log(bonusPoints)
+
+bonusPoints = bonusPoints - 75
+console.log(bonusPoints)
+
+bonusPoints = bonusPoints + 45
+console.log(bonusPoints)
