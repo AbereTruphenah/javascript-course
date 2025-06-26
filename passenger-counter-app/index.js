188c032b4043f987f3e8ed5f623b56d28867ce3d
Truphenah <145029217+AbereTruphenah@users.noreply.github.com>
2025-06-26 08:53:00 +0300

    Added a function that logs sum

--- passenger-counter-app/index.js
+++ passenger-counter-app/index.js
@@ -1,6 +1,10 @@
-// Create a function (you decide the name) that logs out the number 42 to the console
-// Call/invoke the function
-function number(){
-    console.log(42)
+let lap1 = 34
+let lap2 = 33
+let lap3 = 36
+
+// Create a function that logs out the sum of all the lap times
+function lapTimes(){
+    let sum = lap1 + lap2 + lap3
+    console.log(sum)
 }
-number()
+lapTimes()
