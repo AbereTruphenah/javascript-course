e35cb6c03c2d4119addfcafb309ad1ba5805624a
Truphenah <145029217+AbereTruphenah@users.noreply.github.com>
2025-06-26 21:45:19 +0300

    concatenate two strings in a function

--- practice-one/index.js
+++ practice-one/index.js
@@ -1,12 +1,8 @@
-// Create two variables, firstName and lastName
+let name = "Linda"
+let greeting = "Hi there"
 
-// Concatenate the two variables into a third variable called fullName
-
-// Log fullName to the console
-
-
-let firstName = "Abere"
-let lastName = "Truphi"
-
-let fullName = firstName + " " + lastName
-console.log(fullName)
+// Create a function that logs out "Hi there, Linda!" when called
+function greet(){
+    console.log(greeting + ", " + name + "!")
+}
+greet()
