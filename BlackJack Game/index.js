7dd7b1c30e1bfe555549b7c68745d9df61ce3906
Truphenah <145029217+AbereTruphenah@users.noreply.github.com>
2025-06-27 09:26:51 +0300

    Booleans

--- BlackJack Game/index.js
+++ BlackJack Game/index.js
@@ -1,16 +1,17 @@
 let firstCard = 10
-let secondCard = 7
-let sum = firstCard + secondCard + 4
+let secondCard = 11
+let sum = firstCard + secondCard
+let hasBlackJack = false
+// 1. Create a variable called isAlive and assign it to true
 
-// Write the conditional according to these rules:
-
-// if less than or equal to 20 -> "Do you want to draw a new card? 🙂"
-// else if exactly 21 -> "Wohoo! You've got Blackjack! 🥳"
-// else -> "You're out of the game! 😭"
+// 2. Flip its value to false in the appropriate code block 
 if (sum <= 20) {
     console.log("Do you want to draw a new card? 🙂")
 } else if (sum === 21) {
     console.log("Wohoo! You've got Blackjack! 🥳")
+    hasBlackJack = true
 } else {
     console.log("You're out of the game! 😭")
 }
+
+
