d42e7d6d0a4613d2060a49bee7bd69cb45eec1e4
Truphenah <145029217+AbereTruphenah@users.noreply.github.com>
2025-06-27 09:21:26 +0300

    if...else statement for the game

--- BlackJack Game/index.js
+++ BlackJack Game/index.js
@@ -1,12 +1,13 @@
-// 1. Create two variables, firstCard and secondCard. 
-// Set their values to a random number between 2-11
-
-// 2. Create a variable, sum, and set it to the sum of the two cards
 let firstCard = 10
 let secondCard = 7
-let sum = firstCard + secondCard + 6 // 23
+let sum = firstCard + secondCard + 4
 
-if (sum < 21) {
+// Write the conditional according to these rules:
+
+// if less than or equal to 20 -> "Do you want to draw a new card? 🙂"
+// else if exactly 21 -> "Wohoo! You've got Blackjack! 🥳"
+// else -> "You're out of the game! 😭"
+if (sum <= 20) {
     console.log("Do you want to draw a new card? 🙂")
 } else if (sum === 21) {
     console.log("Wohoo! You've got Blackjack! 🥳")
