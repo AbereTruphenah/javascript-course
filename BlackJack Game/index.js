e1450a696343cd15a2ef13a095745429d1a336e7
Truphenah <145029217+AbereTruphenah@users.noreply.github.com>
2025-06-27 09:32:58 +0300

    Practice boolean conditions

--- BlackJack Game/index.js
+++ BlackJack Game/index.js
@@ -1,17 +1,7 @@
-let firstCard = 10
-let secondCard = 11
-let sum = firstCard + secondCard
-let hasBlackJack = false
-// 1. Create a variable called isAlive and assign it to true
-
-// 2. Flip its value to false in the appropriate code block 
-if (sum <= 20) {
-    console.log("Do you want to draw a new card? 🙂")
-} else if (sum === 21) {
-    console.log("Wohoo! You've got Blackjack! 🥳")
-    hasBlackJack = true
-} else {
-    console.log("You're out of the game! 😭")
-}
-
-
+console.log(4 === 3)  // False
+console.log(5 > 2)    // True
+console.log(12 > 12)  // False
+console.log(3 < 0)    // False
+console.log(3 >= 3)   // True
+console.log(11 <= 11) // True
+console.log(3 <= 2)   // False
